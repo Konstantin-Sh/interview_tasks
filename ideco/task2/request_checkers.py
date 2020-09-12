3f85424cc3b56e36a13f6663e31d8953e6126b15
Konstantin-Sh <kos.shevchenko@gmail.com>
2020-09-12 14:53:22 +0500

    Refactor and change function to correct

--- ideco/task2/request_checkers.py
+++ ideco/task2/request_checkers.py
@@ -11,14 +11,7 @@ def is_valid_tcp_upd_port(port):
 # https://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python
 def is_valid_ipv4_address(address):
     try:
-        socket.inet_pton(socket.AF_INET, address)
-    except AttributeError:  # no inet_pton here, sorry
-        try:
-            socket.inet_aton(address)
-        except socket.error:
-            return False
-#        return address.count('.') == 3
-        return True
+        socket.inet_aton(address)
     except socket.error:  # not a valid address
         return False
     return True
